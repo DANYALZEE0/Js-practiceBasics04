183afd044a5819d5663049f3730943988f0fc83b
Zeeshan Patel <89648030+DANYALZEE0@users.noreply.github.com>
2023-12-27 11:01:53 +0530

    Add files via upload

---
+++ truthy.js
@@ -0,0 +1,38 @@
+const userEmail = []
+
+// if (userEmail) {
+//     console.log("Got user email");
+// } else{
+//     console.log("Don't have user email");
+// }
+
+// falsy values
+
+// false, 0, -0, BigInt = 0n, "", null, undefined, NaN
+
+// truthy Values
+//  "0", 'false', " ", [], {}, function(){}
+
+
+// if (userEmail.length  === 0) {
+//     console.log("Array is empty");   
+// }
+
+
+// const emptyObj = {}
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("No Keys avalabel");
+// }
+
+// compatetive truthy value
+/*
+false == 0
+true
+
+false == ''
+true
+
+0 == ''
+true
+*/
+
